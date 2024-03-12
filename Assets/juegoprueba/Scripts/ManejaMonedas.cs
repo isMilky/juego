ac0c7d30d29e9b87d6edf4da59258fa22e3bb09f
isMilky <83434364+isMilky@users.noreply.github.com>
2024-03-11 22:41:47 -0500

    Actualización

---
+++ Assets/juegoprueba/Scripts/ManejaMonedas.cs
@@ -0,0 +1,29 @@
+using MoreMountains.CorgiEngine;
+using MoreMountains.Tools;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManejaMonedas : MonoBehaviour, MMEventListener<PickableItemEvent>
+{
+    private int Monedas_recolectadas()
+    { }
+    void OnEnable()
+    {
+        this.MMEventStartListening<PickableItemEvent>();
+    }
+    void OnDisable()
+    {
+        this.MMEventStopListening<PickableItemEvent>();
+    } 
+    public virtual void onMMEvent(PickableItemEvent pickableitemevent)
+    {
+
+    }
+}
+        
+
+
+   
+
+}
