dd5498ca1e8ef38f5d5841baf260415827116705
isMilky <83434364+isMilky@users.noreply.github.com>
2024-03-20 23:32:36 -0500

    Add files via upload

---
+++ Assets/Scripts/Botones/BotonSalir.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotonSalir : MonoBehaviour
+{
+    public void OnclickExit()
+    {
+        Debug.Log("No hay salida...");
+    }
+}
---
+++ Assets/Scripts/Botones/BottonInst.cs
@@ -0,0 +1,13 @@
+using MoreMountains.Tools;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottonInst : MonoBehaviour
+{
+    public void OnclickInst()
+    {
+        MMSceneLoadingManager.LoadScene("Inst");
+        Debug.Log("Instrucciones");
+    }
+}
